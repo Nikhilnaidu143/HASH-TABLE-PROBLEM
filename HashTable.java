592e761a9f6fbeb5100915e8aa51bf4b403286ca
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-01 06:44:25 +0530

    START:- Displayed welcome message.

---
+++ HashTable.java
@@ -0,0 +1,8 @@
+package com.hash.table;
+
+public class HashTable {
+	public static void main(String[] args) {
+		/** Displayed welcome message. **/
+		System.out.println("-------------WELCOME TO HASH TABLE-------------\n");
+	}
+}
